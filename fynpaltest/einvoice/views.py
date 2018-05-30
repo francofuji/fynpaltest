0129492bca98ac220abec345509d79fdf24e81b3
Francisco Perez <francofuji@gmail.com>
2018-05-30 03:57:57 -0400

    adding views

--- fynpaltest/einvoice/views.py
+++ fynpaltest/einvoice/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import FacturadorSerializer, FacturaElectronicaSerializer
+from .models import Facturador, FacturaElectronica
 
-# Create your views here.
+
+class CreateFacturadorView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Facturador.objects.all()
+    serializer_class = FacturadorSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save()
+
+class CreateFacturaView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = FacturaElectronica.objects.all()
+    serializer_class = FacturaElectronicaSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save()

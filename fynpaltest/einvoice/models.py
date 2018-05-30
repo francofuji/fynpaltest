1d8e4b38aa05461f32370a18af86feb83f40786c
Francisco Perez <francofuji@gmail.com>
2018-05-30 00:21:36 -0400

    fixing models definitions bugs

--- fynpaltest/einvoice/models.py
+++ fynpaltest/einvoice/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Facturador(models.Model):
 	"""docstring for Facturador"""
+
 	rut = models.CharField(max_length = 10)
 	razon_social = models.CharField(max_length = 50)
 
@@ -11,13 +12,14 @@ class Facturador(models.Model):
 
 class FacturaElectronica(models.Model):
 	"""docstring for FacturaElectronica"""
+	
 	id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	dte_version = models.CharField(max_length = 4)
 	document_id = models.CharField(max_length = 10)
 	folio = models.PositiveIntegerField()
 	fecha_emision = models.DateField()
-	emisor = models.ForeingKey(Facturador, related_name = "emitidas", on_delete = models.CASCADE)
-	receptor = models.ForeingKey(Facturador, related_name = "recibidas", on_delete = models.CASCADE)
+	emisor = models.ForeignKey(Facturador, related_name = "emitidas", on_delete = models.CASCADE)
+	receptor = models.ForeignKey(Facturador, related_name = "recibidas", on_delete = models.CASCADE)
 	tasa_iva = models.DecimalField(max_digits=5, decimal_places=2)
 	monto_neto = models.IntegerField()
 	extra_data = models.CharField(max_length = 50)

75e8061b06dfcffef57a00d36a399d8ba4d94416
Francisco Perez <francofuji@gmail.com>
2018-05-30 12:28:00 -0400

    fixing bug

--- fynpaltest/config/urls.py
+++ fynpaltest/config/urls.py
@@ -6,5 +6,5 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name = "home.html"), name='home'),
     url(r'^admin/', admin.site.urls),
-    url(r'^api/', include('einvoice.urls')) # Add this line
+    url(r'^api/', include('einvoice.urls', namespace = "einvoice")) # Add this line
 ]

b4296e21321f577400df6394d6498c40dc03d3d6
Francisco Perez <francofuji@gmail.com>
2018-05-31 23:21:57 +0000

    added basic auth to views

--- fynpaltest/einvoice/views.py
+++ fynpaltest/einvoice/views.py
@@ -1,3 +1,6 @@
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework import generics
 from .serializers import FacturadorSerializer, FacturaElectronicaSerializer
 from .models import Facturador, FacturaElectronica
@@ -8,15 +11,36 @@ class CreateFacturadorView(generics.ListCreateAPIView):
     queryset = Facturador.objects.all()
     serializer_class = FacturadorSerializer
 
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+
     def perform_create(self, serializer):
         """Save the post data when creating a new bucketlist."""
         serializer.save()
+
+    def get(self, request, format=None):
+        content = {
+            'user': str(request.user),  
+            'auth': str(request.auth),  
+        }
+        return Response(content)
+
 
 class CreateFacturaView(generics.ListCreateAPIView):
     """This class defines the create behavior of our rest api."""
     queryset = FacturaElectronica.objects.all()
     serializer_class = FacturaElectronicaSerializer
 
+    authentication_classes = (SessionAuthentication, BasicAuthentication)
+    permission_classes = (IsAuthenticated,)
+
     def perform_create(self, serializer):
         """Save the post data when creating a new bucketlist."""
         serializer.save()
+
+    def get(self, request, format=None):
+        content = {
+            'user': str(request.user),  
+            'auth': str(request.auth),  
+        }
+        return Response(content)
